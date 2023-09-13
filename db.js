f4dd54fabf8d0278c0d9586104d7cecf735e7eb4
Akumalla Srinivasa Sanjay <99539782+sanju-1820@users.noreply.github.com>
2023-09-13 23:33:59 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoDBURL = 'mongodb+srv://rms:rms@atlascluster.e3lkzmc.mongodb.net/mern-rooms'
+
+mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var dbconnect = mongoose.connection
+
+dbconnect.on('error' , ()=>{
+    console.log(`Mongo DB Connection Failed`);
+})
+
+dbconnect.on('connected' , ()=>{
+    console.log(`Mongo DB Connection Successfull`);
+})
+
+module.exports = mongoose
